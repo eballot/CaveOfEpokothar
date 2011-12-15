3d09fb28d1382d531cf16d9442731641db75886d
Ed Ballot <eballot@gmail.com>
2011-12-15 11:24:54 -0800

    better implementation of bind

--- Utils/Utils.js
+++ Utils/Utils.js
@@ -1,7 +1,21 @@
-Function.prototype.bind = Function.prototype.bind || function(scope) {
-	var _function = this;
-	return function() {
-		return _function.apply(scope, arguments);
-	};
-};
+// This function was cribbed from https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function (oThis) {
+		if (typeof this !== "function") {
+			// closest thing possible to the ECMAScript 5 internal IsCallable function
+			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+		}
 
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+		fToBind = this,
+		fNOP = function () {},
+		fBound = function () {
+			return fToBind.apply(this instanceof fNOP ? this : oThis || window, aArgs.concat(Array.prototype.slice.call(arguments)));
+		};
+
+		fNOP.prototype = this.prototype;
+		fBound.prototype = new fNOP();
+
+		return fBound;
+	};
+}
